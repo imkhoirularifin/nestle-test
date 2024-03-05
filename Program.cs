da565fe0490b571e96c083c43e3170d15c678288
khoirul arifin <khoirularfin12@gmail.com>
2024-03-06 05:56:38 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿static string DecimalToBinaryRecursive(int decimalNumber)
+{
+    if (decimalNumber == 0)
+        return "";
+
+    int remainder = decimalNumber % 2;
+    int quotient = decimalNumber / 2;
+
+    return DecimalToBinaryRecursive(quotient) + remainder;
+}
+
+Console.WriteLine("Nestle Test");
+Console.WriteLine("Moh Khoirul Arifin Batch 13 Online");
+Console.WriteLine();
+
+Console.Write("Enter a decimal number: ");
+int decimalNum = Convert.ToInt32(Console.ReadLine());
+
+var binaryNum = DecimalToBinaryRecursive(decimalNum);
+Console.WriteLine("Binary number: " + binaryNum);
